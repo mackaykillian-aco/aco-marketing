a6af1f3f7c0ed0f4f93e3cabe9efee07995ef8db
MacKay Killian <mackay.killian@awardco.com>
2025-11-25 13:47:27 -0700

    Reformated fpa data template, and separated functions into 1. initializeing cookie, and 2. populating ATTR (UTM) values

--- fpa-data.js
+++ fpa-data.js
@@ -1,68 +1,86 @@
-function genFpaDataInitValue(value) {
-  value.cid = crypto.randomUUID();
-  value.vts = Date.now();
-  value.lvs = Date.now();
-
-  // Generate UTM Data
-  var queryString = window.location.search;
-  var urlParams = new URLSearchParams(queryString);
-
-  value.attr.src = urlParams.get("utm_source");
-  value.attr.med = urlParams.get("utm_medum");
-  value.attr.cmp = urlParams.get("utm_campaign");
-  value.attr.trm = urlParams.get("utm_term");
-  value.attr.kwd = urlParams.get("utm_term");
-  value.attr.cnt = urlParams.get("utm_content");
-
-  return value;
-}
-
 var fpaDataTemplate = {
-  cid: 0,
-  vts: 0, // first visit
-  lvs: 0, // most recent session start
-  attr: {
-    src: "",
-    med: "",
-    cmp: "",
-    trm: "",
-    kwd: "",
-    cnt: "",
-    gac: "",
-  },
-  expt: {
-    eid: "",
-    ena: "",
-    etp: "",
-    vid: "",
-    vna: "",
-  },
+  cid: "",
+  fact: 0, // first activity
+  lact: 0, // last activity
+  ga_cid: "", // GA Client ID
+  hsu_id: "", // Hubspotut ID
+  wf_attr: "", // Webflow custom attribute (user level)
   ses: [
     {
-      sid: 1,
-      pgc: 0,
-      tsos: 0,
-      lpv: "",
-      pvh: [
+      sid: "", // Session ID
+      pgc: 0, // Page count
+      tsos: 0, // Time spent on site
+      ldp: "", // Landing page (session entry point)
+      cvp: "", // Conversion page (session conversion point)
+      ref: "", // Referring URL
+      attr: {
+        src: "", // UTM Source
+        med: "", // UTM Medium
+        cmp: "", // UTM Campaign
+        trm: "", // UTM Term
+        kwd: "", // UTM Keyword
+        cnt: "", // UTM Content
+      },
+      ads: {
+        gcl_id: "", // GCLID (Google Ads)
+        dcl_id: "", // DCLID (Doubleclick Ads)
+        msft_id: "", // Microsoft Ads ID
+        lnkd_id: "", // LinkedIn Ads ID
+        meta_id: "", // Meta Ads ID
+      },
+      pvs: [
         {
-          ts: 0,
-          url: "",
-          ttl: "",
-          top: 0,
+          path: "", // Page path
+          ttl: "", // Page title
+          top: "", // Time on page
+          expt: {
+            eid: "", // Webflow Optimize Experiment ID
+            ena: "", // Webflow Optimize Experiment Name
+            etp: "", // Webflow Optimize Experiment Type
+            vid: "", // Webflow Optimize Variant ID
+            vna: "", // Webflow Optimize Variant Name
+          },
         },
       ],
     },
   ],
 };
 
-if (!Cookies.get("_fpa_data")) {
-  var value = JSON.stringify(genFpaDataInitValue(fpaDataTemplate));
-  Cookies.set("_fpa_data", value, { expires: 365, path: "/" });
-} else {
-  var cc = Cookies.get("_fpa_data");
-  cc = JSON.parse(cc);
-  cc.lvs = Date.now();
-  Cookies.set("_fpa_data", JSON.stringify(cc));
+// Init FPA DATA Cookie
+function initFpaDataCookie() {
+  // Check if _fpa_data cookie exists
+  if (!Cookies.get("_fpa_data")) {
+    var value = structuredClone(fpaDataTemplate);
+    value.cid = crypto.randomUUID();
+    value.vts = Date.now();
+    value.lvs = Date.now();
+
+    var value = JSON.stringify(value);
+    Cookies.set("_fpa_data", value, { expires: 365, path: "/" });
+  }
 }
 
-console.log("GitHub Repo Change Test");
+// 1. Populate ATTR Values
+function populateAttrValues() {
+  var value = JSON.parse(Cookies.get("_fpa_data"));
+
+  var queryString = window.location.search;
+  var urlParams = new URLSearchParams(queryString);
+
+  value.ses[0].attr.src = urlParams.get("utm_source");
+  value.ses[0].attr.med = urlParams.get("utm_medium");
+  value.ses[0].attr.cmp = urlParams.get("utm_campaign");
+  value.ses[0].attr.trm = urlParams.get("utm_term");
+  value.ses[0].attr.kwd = urlParams.get("utm_keyword");
+  value.ses[0].attr.cnt = urlParams.get("utm_content");
+
+  Cookies.set("_fpa_data", JSON.stringify(value), { expires: 365, path: "/" });
+}
+
+// 2. Populate EXPT Values
+
+// 3. Populate SES Values
+
+// Execute Functions
+initFpaDataCookie();
+populateAttrValues();
