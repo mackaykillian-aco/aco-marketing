1dbbe8ff7cf19afb57612b50252e8bea646b6141
MacKay Killian <mackay.killian@awardco.com>
2025-11-26 16:37:28 -0700

    Added Logic for fpaData to Form
    Co-authored-by: Michael Lee <michaellee2245@users.noreply.github.com>

--- fpa-data-to-form.js
+++ fpa-data-to-form.js
@@ -1,19 +1,32 @@
 var Webflow = Webflow || [];
 Webflow.push(function () {
-  const cookieValue = JSON.parse(Cookies.get("_fpa_data"));
+  document.querySelectorAll("[hs-form]").forEach((form) => {
+    let fpaDataMapToFormField = {
+      gcl_id: window.fpaData?.ses[0].ads.gcl_id || "",
+      dcl_id: window.fpaData?.ses[0].ads.dcl_id || "",
+      ga_cid: window.fpaData?.ga_cid || "",
+      hsu_id: window.fpaData?.hsu_id || "",
+      msft_id: window.fpaData?.ses[0].ads.msft_id || "",
+      lnkd_id: window.fpaData?.ses[0].ads.lnkd_id || "",
+      meta_id: window.fpaData?.ses[0].ads.meta_id || "",
+      utm_source: window.fpaData?.ses[0].attr.src || "",
+      utm_medium: window.fpaData?.ses[0].attr.med || "",
+      utm_campaign: window.fpaData?.ses[0].attr.cmp || "",
+      utm_term: window.fpaData?.ses[0].attr.trm || "",
+      utm_content: window.fpaData?.ses[0].attr.cnt || "",
+      utm_keyword: window.fpaData?.ses[0].attr.kwd || "",
+      ref_url: window.fpaData?.ses[0].ref || "",
+      landing_page: window.fpaData?.ses[0].ldp || "",
+      convert_url: window.fpaData?.ses[0].cpv || "",
+      last_page_before_demo: window.fpaData?.ses[0].pvs[1].path || "",
+      webflow_form_id: form.getAttribute("name") || "",
+      hubspot_form_id: form.getAttribute("hs-form") || "",
+      // optimize_variant_id: window.fpaData?.ses[0].pvs[0].expt.vid || "",
+      // optimize_exp_id: window.fpaData?.ses[0].pvs[0].expt.eid || "",
+    };
 
-  const cookieValueUTM = cookieValue.ses[0].attr;
-
-  let UTMs = {
-    utm_source: cookieValueUTM.src || "",
-    utm_medium: cookieValueUTM.med || "",
-    utm_campaign: cookieValueUTM.cmp || "",
-    utm_term: cookieValueUTM.trm || "",
-    utm_content: cookieValueUTM.cnt || "",
-    utm_keyword: cookieValueUTM.kwd || "",
-  };
-
-  Object.keys(UTMs).forEach((key) => {
-    $(`[hs-form-field="${key}"]`).val(UTMs[key]);
+    Object.keys(fpaDataMapToFormField).forEach((key) => {
+      $(form).find(`[hs-form-field="${key}"]`).val(fpaDataMapToFormField[key]);
+    });
   });
 });
